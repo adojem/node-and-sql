54b7a854e9cface280c6eda4c42947f6f4d7b396
adojem <adojemcovix@gmail.com>
2018-07-28 16:13:55 +0900

    Seeding & Models

--- database/migrations/1503248427885_user.js
+++ database/migrations/1503248427885_user.js
@@ -5,10 +5,13 @@ const Schema = use('Schema')
 class UserSchema extends Schema {
   up () {
     this.create('users', (table) => {
+      // table.integer('id').notNullable().unsigned().primary();
       table.increments()
       table.string('username', 80).notNullable().unique()
-      table.string('email', 254).notNullable().unique()
+      table.string('email', 100).notNullable().unique()
       table.string('password', 60).notNullable()
+      table.string('profile_img', 200).notNullable().defaultTo('/uploads.default.jpg');
+      table.string('bio', 255);
       table.timestamps()
     })
   }
